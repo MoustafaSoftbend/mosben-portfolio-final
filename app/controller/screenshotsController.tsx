b89b3ad573a8cd279ba18aff38e4bb6a1ab8bc1f
Moustafa Bendib <46543561+MoustafaSoftbend@users.noreply.github.com>
2024-07-04 14:50:18 +0000

    screenshotsController.tsx, adjust your error handling to use AxiosError properly

--- app/controller/screenshotsController.tsx
+++ app/controller/screenshotsController.tsx
@@ -1,48 +1,40 @@
-"use client";
-
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export const screenshotsController = async (
   url: string,
-  foldername: string = "default",
+  foldername = "default",
 ) => {
-  let screens: any = {};
+  let screens = {};
+  let response;
 
   try {
-    const screensExist = await getScreens();
-
-    if (screensExist) {
-      console.log("Screens already exist:", screensExist);
-      screens = screensExist;
+    response = await get_screens();
+    if (response.status === 200) {
+      console.log(response);
+      screens = response.data;
       return screens;
     }
-
-    const createdScreens = await createScreens(url, foldername);
-
-    if (createdScreens) {
-      console.log("Screens created successfully:", createdScreens);
-      screens = createdScreens;
-      return screens;
-    }
-
-    return screens;
   } catch (error) {
-    console.error("Error in screenshotsController:", error);
-    return screens; // Return empty screens or handle error as per your application logic
+    response = await createScreens(url, foldername);
+    if (response.status === 200) {
+      console.log(response);
+      screens = response.data;
+      return screens;
+    }
   }
+  return screens;
 };
 
-const getScreens = async () => {
+const get_screens = async () => {
   try {
-    const response = await axios.get("/api/images");
+    const response = await axios.get("api/images");
     return response.data; // Assuming response.data contains the screens or an indication of existence
-  } catch (error: unknown) {
+  } catch (error) {
     if (axios.isAxiosError(error)) {
       const axiosError = error as AxiosError;
       if (axiosError.response && axiosError.response.status === 404) {
         return false; // Screens do not exist
       }
-      throw axiosError;
     }
     throw error;
   }
@@ -53,16 +45,16 @@ const createScreens = async (url: string, foldername: string) => {
     const response = await axios.post("/api/images", {
       url: url,
       fullPage: true,
-      foldername: foldername,
+      foldername,
     });
 
     if (response.status === 200) {
-      return response.data; // Assuming response.data contains the created screens
+      return response.data;
     } else {
       throw new Error("Failed to create screens");
     }
   } catch (error) {
-    console.error("Error creating screens:", error);
+    console.error(error);
     throw error;
   }
 };
