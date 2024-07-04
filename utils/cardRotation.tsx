8c0875cd73e25f55b9c3710e50e015403833ca20
Moustafa Bendib <46543561+MoustafaSoftbend@users.noreply.github.com>
2024-07-04 20:37:57 +0000

    fixed card rotation typs

--- utils/cardRotation.tsx
+++ utils/cardRotation.tsx
@@ -1,16 +1,19 @@
 export const cardRotation = () => {
-  const cards = document.querySelectorAll(".profile");
+  const cards = document.querySelectorAll<HTMLElement>(".profile");
 
-  const observerOptions = {
+  const observerOptions: IntersectionObserverInit = {
     root: null, // Use the viewport as the root
     rootMargin: "0px 1000px 0px 1000px", // No margin around the root
-    threshold: 0.1, // Trigger when 50% of the observed element is visible
+    threshold: 0.1, // Trigger when 10% of the observed element is visible
   };
-  const intersection = (entries) => {
+
+  // Define the type for the entries parameter
+  const intersection: IntersectionObserverCallback = (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.style.opacity = 1;
-        entry.target.style.transform = "rotateY(0deg) translateX(0%)";
+        (entry.target as HTMLElement).style.opacity = 1;
+        (entry.target as HTMLElement).style.transform =
+          "rotateY(0deg) translateX(0%)";
       }
     });
   };
