425c8457be2c80be8d6c1c2b10f4390a2c001370
Moustafa Bendib <46543561+MoustafaSoftbend@users.noreply.github.com>
2024-07-04 19:52:20 +0000

    fixed acc problem

--- lib/cloudinary.tsx
+++ lib/cloudinary.tsx
@@ -27,6 +27,17 @@ export const handleCloudinaryUpload = async ({
   return result;
 };
 
+// Define a type for the resource object
+type Resource = {
+  folder: string;
+  [key: string]: any; // Add other properties if needed
+};
+
+// Define a type for the accumulator object
+type Accumulator = {
+  [key: string]: Resource[];
+};
+
 export const handleGetCloudinaryUploads = async () => {
   const resources = await cloudinary.search
     .expression("resource_type:image")
@@ -34,14 +45,17 @@ export const handleGetCloudinaryUploads = async () => {
     .max_results(500)
     .execute();
 
-  const groupedResources = resources.resources.reduce((acc, resource) => {
-    const folderName = resource.folder;
-    if (!acc[folderName]) {
-      acc[folderName] = [];
-    }
-    acc[folderName].push(resource);
-    return acc;
-  }, {});
+  const groupedResources = resources.resources.reduce<Accumulator>(
+    (acc, resource: Resource) => {
+      const folderName = resource.folder;
+      if (!acc[folderName]) {
+        acc[folderName] = [];
+      }
+      acc[folderName].push(resource);
+      return acc;
+    },
+    {},
+  );
 
   return groupedResources;
 };
