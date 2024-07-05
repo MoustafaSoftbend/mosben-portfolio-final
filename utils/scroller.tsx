18beeb4f5fa6994e4d9b9d1206bc4a27b88d9bf2
Moustafa Bendib <46543561+MoustafaSoftbend@users.noreply.github.com>
2024-07-05 09:28:53 +0000

    fixed null problem on scroller

--- utils/scroller.tsx
+++ utils/scroller.tsx
@@ -1,19 +1,28 @@
 export const scroller = () => {
-  const carousel = document.querySelector(".carousel-container");
-  const slide_right = document.querySelector(".arrow-right");
-  const slide_left = document.querySelector(".arrow-left");
+  const carousel = document.querySelector<HTMLElement>(".carousel-container");
+  const slide_right = document.querySelector<HTMLElement>(".arrow-right");
+  const slide_left = document.querySelector<HTMLElement>(".arrow-left");
 
-  var position = carousel.scrollLeft;
-  const right_pos = carousel.scrollWidth - carousel.clientWidth;
+  if (carousel) {
+    var position = carousel.scrollLeft;
+    const right_pos = carousel.scrollWidth - carousel.clientWidth;
 
-  // console.log(carousel, slide_right, slide_left);
+    // Check if slide_right exists before adding the event listener
+    if (slide_right) {
+      slide_right.addEventListener("click", () => {
+        position >= right_pos ? (position = right_pos) : (position += 150);
+        carousel.scrollLeft = position;
+      });
+    }
 
-  slide_right.addEventListener("click", () => {
-    position >= right_pos ? (position = right_pos) : (position += 150);
-    carousel.scrollLeft = position;
-  });
-  slide_left.addEventListener("click", () => {
-    position == 0 ? (position = 0) : (position -= 150);
-    carousel.scrollLeft = position;
-  });
+    // Check if slide_left exists before adding the event listener
+    if (slide_left) {
+      slide_left.addEventListener("click", () => {
+        position == 0 ? (position = 0) : (position -= 150);
+        carousel.scrollLeft = position;
+      });
+    }
+  } else {
+    console.warn("Carousel element not found");
+  }
 };
