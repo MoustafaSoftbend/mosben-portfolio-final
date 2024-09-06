bf3ba44ae895d589552b5e0bcd05b37a3834b2b0
mosben <bendib.moustafa@gmail.com>
2024-09-06 10:32:13 +0000

    fixed next 13 route handling

--- app/api/nodemailer/route.tsx
+++ app/api/nodemailer/route.tsx
@@ -1,10 +1,9 @@
-// pages/api/sendMail.ts
-
 import nodemailer from "nodemailer";
 import { NextRequest, NextResponse } from "next/server";
 
-export default async function POST(req: NextRequest) {
-    const { to, subject, text, html } = req.body;
+export async function POST(req: NextRequest) {
+  try {
+    const { to, subject, text, html } = await req.json();
 
     // Create a transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
@@ -17,20 +16,18 @@ export default async function POST(req: NextRequest) {
       },
     });
 
-    try {
-      // Send mail with defined transport object
-      await transporter.sendMail({
-        from: process.env.SMTP_FROM, // sender address
-        to, // list of receivers
-        subject, // Subject line
-        text, // plain text body
-        html, // html body
-      });
+    // Send mail with defined transport object
+    await transporter.sendMail({
+      from: process.env.SMTP_FROM, // sender address
+      to, // list of receivers
+      subject, // Subject line
+      text, // plain text body
+      html, // html body
+    });
 
-      transporter.status(200).json({ message: "Email sent successfully" });
-    } catch (error) {
-      console.error(error);
-      transporter.status(500).json({ message: "Failed to send email" });
-    }
-
+    return NextResponse.json({ message: "Email sent successfully" }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Failed to send email" }, { status: 500 });
+  }
 }
