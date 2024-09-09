5dcb527081df443df98265902edcb75eaafd01e7
mosben <bendib.moustafa@gmail.com>
2024-09-09 19:25:28 +0000

    fixed screens type interface

--- app/page.tsx
+++ app/page.tsx
@@ -48,30 +48,31 @@ const Home = () => {
   };
 
   interface Screen {
-    asset_id: string;
-    public_id: string;
-    folder: string;
-    filename: string;
-    format: string;
-    version: number;
-    resource_type: string;
-    type: string;
-    created_at: string;
-    uploaded_at: string;
-    bytes: number;
-    width: number;
-    height: number;
-    aspect_ratio: number;
-    pixels: number;
-    url: string;
-    secure_url: string;
-    status: string;
-    access_mode: string;
-    access_control: null;
-    etag: string;
-    created_by: string;
-    access_key: string;
-    uploaded_by: string;
+    [key: string]: Array<{ secure_url: string }>;
+    // asset_id: string;
+    // public_id: string;
+    // folder: string;
+    // filename: string;
+    // format: string;
+    // version: number;
+    // resource_type: string;
+    // type: string;
+    // created_at: string;
+    // uploaded_at: string;
+    // bytes: number;
+    // width: number;
+    // height: number;
+    // aspect_ratio: number;
+    // pixels: number;
+    // url: string;
+    // secure_url: string;
+    // status: string;
+    // access_mode: string;
+    // access_control: null;
+    // etag: string;
+    // created_by: string;
+    // access_key: string;
+    // uploaded_by: string;
   }
 
   interface StaticData {
