91402dcd8677a0676bcd5a20a87649a98cb23e12
mosben <bendib.moustafa@gmail.com>
2024-09-11 22:12:33 +0000

    fixed url problem

--- app/page.tsx
+++ app/page.tsx
@@ -146,7 +146,7 @@ const Home = () => {
 
     // const urls = ["https://7kwlxf-3000.csb.app/", "https://ygh6gy-3000.csb.app/"];
     if (urlsState.length <= 0) {
-      setUrlsState(["https://7kwlxf-3000.csb.app/", "https://ygh6gy-3000.csb.app/"]);
+      setUrlsState(["https://mosben-portfolio-final-283wxmdfp-moustafasoftbends-projects.vercel.app/", "https://ygh6gy-3000.csb.app/"]);
     }
     // console.log(urlsState);
     // const linkLength = urls.length;
